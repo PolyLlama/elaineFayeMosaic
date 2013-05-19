549c63b5a50c96961fd1e83f33283fb4c2eb7fd0
Matt McCandless <dev@polyllama.com>
2013-05-18 20:41:06 -0500

    removing mondodb binary from repo

--- routes/images.js
+++ routes/images.js
@@ -2,6 +2,6 @@ var url = require('url');
 
 exports.subscription= function(req, res){
   var data = url.parse(req.url, true).query;
-
+  console.log(req.body);
   res.send(data['hub.challenge']);
 };
